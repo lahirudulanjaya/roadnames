ca47f9787837d1156be7c74ff24faaeac04226ed
LahiruDulanjaya <laahirudulanjaya@gmail.com>
2019-06-25 19:53:49 +0530

    first commit

---
+++ src/Data.js
@@ -0,0 +1,34 @@
+
+
+const Data =[{
+    position:{lat: 6.9059928, lng: 79.8708684},
+    roadname:"Vidya Mawatha",
+    description:""
+},
+{
+    position:{lat:6.9161698, lng: 79.8713567},
+    roadname:"Kynsey Road",
+    description:""
+
+},
+{
+    position:{lat: 6.90941, lng: 79.871790},
+    roadname:"Srimath R.G.Senanayake Mawatha",
+    description:"Description ??????"
+
+}
+,
+{
+    position:{lat: 6.911040, lng: 79.87111},
+    roadname:"Wijerama Mawatha",
+    description:"????????????????"
+
+},
+{
+    position:{lat:6.907036,lng:79.862217},
+    roadname:"Premasiri Khemadasa Mawatha",
+    description:"????????????????"
+
+}
+]
+export default Data
